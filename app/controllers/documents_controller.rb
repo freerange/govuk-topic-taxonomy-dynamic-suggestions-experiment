3aa149c6597b706c6153d4f1be80febeb7fc480b
James Mead <james@floehopper.org>
2026-03-17 11:35:36 +0000

    Extract suggested_taxons into controller action

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -6,6 +6,7 @@ class DocumentsController < ApplicationController
   def show
     @document = Document.find(params[:id])
     @similar_documents = @document.nearest_neighbors(:embedding, distance: 'cosine').select(&:published?).first(5)
+    @suggested_taxons = @similar_documents.flat_map(&:taxons)
   end
 
   def new
