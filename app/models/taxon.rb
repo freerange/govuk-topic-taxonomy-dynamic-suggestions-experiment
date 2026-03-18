c532f05500ada9aa3e136ffa3cadcecc11b9ba6b
Chris Roos <chris@seagul.co.uk>
2026-03-18 12:31:46 +0000

    Show breadcrumbs of existing/suggested taxons
    Use the GOV.UK Breadcrumb component[1] to make it clearer where the
    existing/suggested taxons sit in the Topic taxonomy tree.

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -1,2 +1,15 @@
 class Taxon < ApplicationRecord
+  def parent
+    Taxon.find_by(base_path: self.parent_base_path)
+  end
+
+  def breadcrumbs
+    taxon = self
+    taxons = [taxon]
+    while taxon.parent.present?
+      taxon = taxon.parent
+      taxons << taxon
+    end
+    taxons
+  end
 end
